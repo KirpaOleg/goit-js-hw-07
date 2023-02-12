8260deb9d486a918f50d0bf97e27ad02f9303a14
Oleg Kirpa <profix.ok@gmail.com>
2023-02-12 13:46:28 +0200

    2 tasts

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const createGallery = (gallery) => {
+  return gallery
+    .map(({preview, original, description}) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
+    })
+    .join('');
+};
+
+const createGalleryHtml = createGallery(galleryItems);
+galleryEl.insertAdjacentHTML('afterbegin', createGalleryHtml);
+
+function onGalleryClick(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  event.preventDefault();
+
+  const onCloseModal = (event) => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  };
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  window.addEventListener('keydown', onCloseModal);
+}
+
+galleryEl.addEventListener('click', onGalleryClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const createGallery = (gallery) => {
+  return gallery
+    .map(({preview, original, description}) => {
+      return `
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>
+      `;
+    })
+    .join('');
+};
+
+const createGalleryHtml = createGallery(galleryItems);
+galleryEl.insertAdjacentHTML('afterbegin', createGalleryHtml);
+
+let gallery = new SimpleLightbox('.gallery__item', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  navText: ['&#11164', '&#11166'],
+  captionPosition: 'top',
+});
+
+const captionsDataCentr = gallery.domNodes.caption;
+captionsDataCentr.style.textAlign = 'center';
